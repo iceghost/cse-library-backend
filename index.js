fb43a4c292106a2e0c4a3764236dad89f28ba4e9
iceghost <khangemail22@gmail.com>
2022-05-21 21:37:18 +0700

    Add routes

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
+app.post('/login', (req, res) => {
+    // TODO: login user
+});
+
+app.get('/users/:uid', (req, res) => {
+    // TODO: get user information?
+});
+
+app.post('/users/:uid/recept', (req, res) => {
+    // TODO: check-in/check-out user
+});
+
+app.put('/admins/:uid', (req, res) => {
+    // TODO: add user as admin
+});
+
 app.listen(3000, () => {
     console.log(`Example app listening on http://localhost:3000`);
 });
