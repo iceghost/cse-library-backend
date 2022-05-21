65b90871538b99be59886630197c4a12d0949820
iceghost <khangemail22@gmail.com>
2022-05-21 21:16:10 +0700

    Scaffold simple hello world express

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 // @ts-check
-// try check in and check out
-/**
- * @typedef {import('@prisma/client').Student} Student
- * @typedef {import('@prisma/client').Admin} Admin
- */
-const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const express = require('express');
+const app = express();
 
-/**
- * @param {Student} student
- * @param {Admin} admin
- */
-async function recept(student, admin) {
-  await prisma.reception.create({
-    data: {
-      studentId: student.id,
-      adminId: admin.id,
-    },
-  });
-}
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
 
-(async () => {
-  const admins = await prisma.admin.findMany();
-  const admin = () => admins[Math.floor(Math.random() * admins.length)];
-  const A = await prisma.student.findUnique({ where: { id: 3 } });
-  const B = await prisma.student.findUnique({ where: { id: 4 } });
-  await recept(A, admin());
-  await recept(B, admin());
-  await recept(B, admin());
-  await recept(A, admin());
-})();
+app.listen(3000, () => {
+    console.log(`Example app listening on http://localhost:3000`);
+});
