2018355ab194c7e2666590d53af2003e4f55836d
iceghost <khangemail22@gmail.com>
2022-06-15 16:20:46 +0700

    chore: adapt to new schema

--- db/blacklist.js
+++ db/blacklist.js
@@ -1,67 +1,63 @@
-// @ts-check
-
 const client = require('.');
 
 /**
  * @param {number} uid
- * @returns {Promise<import('@prisma/client').Blacklist>}
+ * @param {import('@prisma/client').Admin} author
  */
-
-async function blackListCreate(uid) {
+async function blackListCreate(uid, author) {
     const blacklist = await client.blacklist.create({
         data: {
             studentId: uid,
-            expiredAt: null,      
+            authorId: author.id,
         },
     });
-
     return blacklist;
 }
 
 async function blackListGetAll() {
-    const blacklist = await client.blacklist.findMany({
-        where: {},
+    const blacklist = await client.blacklist.findMany();
+    return blacklist.filter((ele) => {
+        return ele.expiredAt === null || ele.expiredAt > ele.createdAt;
     });
-
-    blacklist.filter(ele => {
-        return ele.expiredAt !== null || ele.expiredAt > ele.createdAt;
-        })
-
-    return blacklist;
 }
 
+/**
+ * @param {number} id
+ */
 async function blackListGetOne(id) {
     const the_student = await client.blacklist.findUnique({
-        where: {
-            studentId: id,
-        },
+        where: { studentId: id },
     });
 
     return the_student;
 }
-async function blackListDeleteOne (id) {
-    const blacklist = await client.blacklist.updateMany({
-        where: {
-            studentId: id,     
-        },
-        data: {
-            expiredAt: new Date (),
-        }
-    })
 
-    
-    // console.log("blacklist", blacklist);
-    return blacklist;
-}
-
-async function blackListDeleteAll() {
-    const blacklist = await client.blacklist.updateMany({
-        where: {},
-        data: {
-            expiredAt: new Date (),
-        }
+/**
+ * @param {number} id
+ * @param {import('@prisma/client').Admin} admin
+ */
+async function blackListDeleteOne(id, admin) {
+    const blacklist = await client.blacklist.update({
+        where: { studentId: id },
+        data: { expiredAt: new Date(), authorId: admin.id },
     });
     return blacklist;
 }
 
-module.exports = {blackListCreate, blackListDeleteAll, blackListGetAll, blackListGetOne, blackListDeleteOne};
+/**
+ * @param {import('@prisma/client').Admin} admin
+ */
+async function blackListDeleteAll(admin) {
+    const blacklist = await client.blacklist.updateMany({
+        data: { expiredAt: new Date(), authorId: admin.id },
+    });
+    return blacklist;
+}
+
+module.exports = {
+    blackListCreate,
+    blackListDeleteAll,
+    blackListGetAll,
+    blackListGetOne,
+    blackListDeleteOne,
+};
