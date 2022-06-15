52685b2505733bbb65d37bda8c30792964847646
iceghost <khangemail22@gmail.com>
2022-06-15 14:55:46 +0700

    feat: update seed to match schema

--- seed.js
+++ seed.js
@@ -1,65 +1,76 @@
-// @ts-check
 // this script is used to pre-populate the database
 // see https://www.prisma.io/docs/guides/database/seed-database
 
 const dummy = () => {
-  const alpha = Array.from(Array(26)).map((_, i) => i + 65);
-  const alphabet = alpha.map((x) => String.fromCharCode(x));
-  console.log('alpha ', alphabet);
-  return alphabet;
+    const alpha = Array.from(Array(26)).map((_, i) => i + 65);
+    const alphabet = alpha.map((x) => String.fromCharCode(x));
+    console.log('alpha ', alphabet);
+    return alphabet;
 };
 
 const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
 const prisma = new PrismaClient();
 
+/** @param {string} password */
+const hash = async (password) => await bcrypt.hash(password, 10);
+
 async function main() {
-  const khang = await prisma.student.upsert({
-    where: { email: 'khang.nguyenduycse@hcmut.edu.vn' },
-    update: {},
-    create: {
-      email: 'khang.nguyenduycse@hcmut.edu.vn',
-      id: 101,
-      fname: 'Khang',
-      lname: 'ND',
-      admin: {
-        create: {},
-      },
-    },
-  });
-
-  const nghi = await prisma.student.upsert({
-    where: { email: 'nghi.nguyen1805vt@hcmut.edu.vn' },
-    update: {},
-    create: {
-      email: 'nghi.nguyen1805vt@hcmut.edu.vn',
-      id: 100,
-      fname: 'Nghi',
-      lname: 'NDP',
-      admin: {
-        create: {},
-      },
-    },
-  });
-
-  for (const x of dummy()) {
-    const student = await prisma.student.upsert({
-      where: { email: `${x}@hcmut.edu.vn` },
-      update: {},
-      create: {
-        email: `${x}@hcmut.edu.vn`,
-        id: x.charCodeAt(0),
-        fname: `${x}`,
-        lname: `lname ${x}`,
-      },
+    const khang = {
+        email: 'khang.nguyenduycse@hcmut.edu.vn',
+        id: 101,
+        fname: 'Khang',
+        lname: 'ND',
+        password: await hash('khang1234'),
+    };
+    await prisma.student.upsert({
+        where: { email: khang.email },
+        update: khang,
+        create: { ...khang, admin: { create: {} } },
     });
-  }
+
+    const nghi = {
+        email: 'nghi.nguyen1805vt@hcmut.edu.vn',
+        id: 100,
+        fname: 'Nghi',
+        lname: 'NDP',
+        password: await hash('nghi1234'),
+    };
+    await prisma.student.upsert({
+        where: { email: nghi.email },
+        update: nghi,
+        create: { ...nghi, admin: { create: {} } },
+    });
+
+    for (const x of dummy()) {
+        const student = {
+            email: `${x}@hcmut.edu.vn`,
+            id: x.charCodeAt(0),
+            fname: `${x}`,
+            lname: `lname ${x}`,
+            password: await hash(`${x}1234`),
+        };
+        await prisma.student.upsert({
+            where: { email: student.email },
+            update: student,
+            create: student,
+        });
+    }
+
+    for (let seatId = 0; seatId <= 55; seatId++) {
+        await prisma.seat.upsert({
+            where: { id: seatId },
+            update: {},
+            create: { id: seatId },
+        });
+    }
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
